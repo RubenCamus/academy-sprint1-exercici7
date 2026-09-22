fe46a00ab0cf4e27c65fc4ec680ef7b22adb1505
RubenCamus <Ruben-4-millet@hotmail.com>
2026-09-22 10:18:07 +0200

    fix: Exception type

--- nivell1-2-3/exercici2.php
+++ nivell1-2-3/exercici2.php
@@ -5,16 +5,16 @@ function validateForm() {
     $email = $_POST["email"];
     $phone = $_POST["phone"];
     if (gettype($username) != "string") {
-        throw new Exception("username is not type string");
+        throw new InvalidArgumentException("username is not type string");
     }
     if (preg_match("/^[a-zA-Z0-9_-]{3,20}$/", $username) === 0) {
-        throw new Exception("Not a valid username. Only A-z, numbers, - _. Min 3 chars, Max 20 chars.");
+        throw new InvalidArgumentException("Not a valid username. Only A-z, numbers, - _. Min 3 chars, Max 20 chars.");
     }
     if (gettype($email) != "string") {
-        throw new Exception("Not a valid email");
+        throw new InvalidArgumentException("Not a valid email");
     }
     if (preg_match("/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/", $email) === 0) {
-        throw new Exception("Not a valid email.");
+        throw new InvalidArgumentException("Not a valid email.");
     }
 }
 try {
